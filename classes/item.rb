4c7a614a657d01a1006a4ced0d426a297a98a93e
hana <hanasabih@gmail.com>
2023-05-03 18:56:51 +0300

    fix linters

--- classes/item.rb
+++ classes/item.rb
@@ -1,7 +1,7 @@
 require 'date'
 
 class Item
-  attr_reader :id, :publish_date, :archived, :author, :label, :source, :genre
+  attr_reader :id, :publish_date, :archived, :genre
 
   def initialize(publish_date)
     @id = Random.rand(1..1000)
@@ -12,21 +12,6 @@ class Item
   def add_genre(genre)
     @genre = genre
     genre.items << self unless genre.items.include?(self)
-  end
-
-  def add_author(author)
-    @author = author
-    author.items << self unless author.items.include?(self)
-  end
-
-  def add_label(label)
-    @label = label
-    label.items << self unless label.items.include?(self)
-  end
-
-  def add_source(source)
-    @source = source
-    source.items << self unless source.items.include?(self)
   end
 
   def move_to_archive
