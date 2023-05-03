64bf0855cb1377925145faa3e29d1b8ae00d08b9
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-05-03 09:00:53 +0530

    Book class and testing initiate

--- classes/book.rb
+++ classes/book.rb
@@ -1,5 +1,24 @@
-class book
-    def initialize
-        
+class Demo
+    def can_be_archived?
+        false
     end
 end
+
+class Book < Demo
+    attr_accessor :publisher
+    attr_reader :cover_state
+    def initialize(publisher, cover_state)
+        @publisher = publisher
+        @cover_state = cover_state
+    end
+    
+    # private just for testing 
+    def can_be_archived?
+        super || cover_state == "bad" ? true : false
+    end
+end
+
+book1 =  Book.new("CCC", "bad")
+puts book1.can_be_archived?
+book2 =  Book.new("CCC", "good")
+puts book2.can_be_archived?
--- classes/label.rb
+++ classes/label.rb
@@ -33,10 +33,15 @@ class Label
   end
 end
 
-##################################### caution open this at your own risk also for testing  ######################################
+# #################################### caution open this at your own risk also for testing  ######################################
 # label1 = Label.new("action", "red")
+# label1.add_item("new item")
 # label2 = Label.new("drama", "pink")
+# label2.add_item("new item")
 # label3 = Label.new("genre", "cyan")
+# label3.add_item("new item")
 # label4 = Label.new("romance", "yellow")
-# write_labels_to_json_file([label1, label2, label3, label4], "./data/label.json")
-# read_labels_from_json_file("./data/label.json", Label).map {|label| puts label.title, label.color}
+# label4.add_item("new item")
+
+# write_to_json_file([label1, label2, label3, label4], "./data/label.json")
+puts read_from_json_file("./data/label.json", Label)
