e70373c251800b8f2557c6a91d3f0de72074d7e4
hana <hanasabih@gmail.com>
2023-05-03 08:47:12 +0300

    fix linters

--- classes/label.rb
+++ classes/label.rb
@@ -1,15 +1,15 @@
 class Label
-    attr_reader :id, :title, :color, :items, :label
-  
-    def initialize(title, color)
-      @id = Random.rand(1..1000)
-      @title = title
-      @color = color
-      @items = []
-    end
-  
-    def add_item(item)
-      @items << item unless @items.include?(item)
-      item.label = self unless item.label == self
-    end
+  attr_reader :id, :title, :color, :items, :label
+
+  def initialize(title, color)
+    @id = Random.rand(1..1000)
+    @title = title
+    @color = color
+    @items = []
   end
+
+  def add_item(item)
+    @items << item unless @items.include?(item)
+    item.label = self unless item.label == self
+  end
+end
--- menu.rb
+++ menu.rb
@@ -1,11 +1,11 @@
 class Menu
-    MENU_OPTIONS = {
-      '1' => :list_books,
-      '2' => :list_people,
-      '3' => :create_person,
-      '4' => :create_book,
-      '5' => :create_rental,
-      '6' => :list_rentals_for_person,
-      '7' => :exit
-    }.freeze
-  end
+  MENU_OPTIONS = {
+    '1' => :list_books,
+    '2' => :list_people,
+    '3' => :create_person,
+    '4' => :create_book,
+    '5' => :create_rental,
+    '6' => :list_rentals_for_person,
+    '7' => :exit
+  }.freeze
+end
